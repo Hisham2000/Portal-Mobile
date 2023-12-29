9a8951c8911685987778eb5ea3bef77a6e583c56
Hisham_Anwar <hishamanwar72@gmail.com>
2023-12-29 23:54:02 +0200

    First Commit

--- lib/component/mint-navbar.dart
+++ lib/component/mint-navbar.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class MintAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return AppBar(
       title: Image(
         image: AssetImage("images/5.png"),
@@ -11,27 +10,29 @@ class MintAppBar extends StatelessWidget implements PreferredSizeWidget {
         height: 60,
       ),
       actions: [
-        Container(
-          margin: EdgeInsets.only(right: 10),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20.0), // Adjust the radius as needed
-            color:  Color(0xFFFFD700), // Set the background color
-          ),
-          child: TextButton(
-          onPressed: () {
-            // Add your button action here
-            print('Button pressed!');
-          },
-          child: Text(
-            'Free Consolation',
-            style: TextStyle(
-              color: Colors.white,
-              // Set the text color
-            ),
-          ),
-        ),),
-
+        _buildBarButton(),
       ],
+    );
+  }
+
+  _buildBarButton() {
+    return Container(
+      margin: EdgeInsets.only(right: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+        color: Color(0xFFFFD700), // Set the background color
+      ),
+      child: TextButton(
+        onPressed: () {
+          print('Button pressed!');
+        },
+        child: Text(
+          'Free Consolation',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
     );
   }
 
